bc3180d17efd66f8c07d5699344d2c9cfa4244fc
thewolf3304 <santiago.barboza@tecsup.edu.pe>
2023-04-12 13:47:14 -0500

    Configuracion de conexion.php

--- conexion.php
+++ conexion.php
@@ -1,11 +1,14 @@
 <?php
 	function conexion(){
-	$host = "host=containers-us-west-41.railway.app";
-	$port = "port=6953";
+	
+	$host = "host=containers-us-west-15.railway.app";
+	$port = "port=7202";
 	$dbname = "dbname=railway";
 	$user = "user=postgres";
-	$password = "password=gx48a5ALGCFUHSn4bfGb";
+	$password = "password=RkP4wrSKnzuAs3v3YuRA";
+
 	$db = pg_connect("$host $port $dbname $user $password");
-    return $db;
+    
+	return $db;
 }
 ?>
